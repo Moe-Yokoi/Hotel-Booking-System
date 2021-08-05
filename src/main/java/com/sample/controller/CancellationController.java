8a668327433179d07bc71bb6a56a693cb269173a
Moe <momo_yuzu_77@yahoo.co.jp>
2021-08-04 17:00:38 -0700

    added list on cancellation page

--- src/main/java/com/sample/controller/CancellationController.java
+++ src/main/java/com/sample/controller/CancellationController.java
@@ -1,5 +1,7 @@
 package com.sample.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,7 +25,11 @@ public class CancellationController {
 	public String displayBookingPage(Model model) {
       model.addAttribute("customer", new CustomerInfo());
 	
-	  return "forms/request-cancellation";
+      List<CustomerInfo> requestedInfo = infoRepo.findAll();
+      model.addAttribute("requestedInfo", requestedInfo);
+      
+//	  return "forms/request-cancellation";
+      return "forms/cancel";
 	}
 	
 //	@PostMapping("/requestedinfo")
@@ -35,7 +41,7 @@ public class CancellationController {
 	
 	
 	@PostMapping("/complete")
-	  public String delete(@RequestParam long bookingNumber) {
+	  public String delete(@RequestParam long bookingNumber) { 
 	    infoRepo.deleteById(bookingNumber);
 	    return "messages/cancellation-successful";
 	  }
